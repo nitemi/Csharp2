61ee47d0a8e650cc17226f33ffd491d8df1bfa62
nitemi@ <blessedtgirl@gmail.com>
2023-12-04 11:18:06 +0100

    Add project files.

---
+++ Csharp/Party.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp
+{
+    public delegate bool Partyparty(Party party);
+    public class Party
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        
+    }
+}
---
+++ Csharp/Program.cs
@@ -0,0 +1,63 @@
+﻿using Csharp;
+public class Program
+{
+    //Icollection
+    //dictionary
+    //list, stack, queue and set
+    private static void Main(string[] args)
+    {
+        
+        Party party1 = new Party()
+        {
+          Id = 1,
+         Price = 100000,
+         Name = "Birthday Celebration",
+         Description = "Ade's birthday celebration"
+            
+        };
+        Party party2 = new Party()
+        {
+            Id = 2,
+            Price = 200000,
+            Name = "Naming Ceremony",
+            Description = "Mr & Mrs Adeyi naming celebration"
+
+        };
+        Party party3 = new Party()
+        {
+            Id = 3,
+            Price = 450000,
+            Name = "Corporate Event ",
+            Description = "Walure Mananagement Event"
+        };
+        Party party4 = new Party()
+        {
+            Id = 4,
+            Price = 250000,
+            Name = "House Warming",
+            Description = "Blessed House Opening",
+        };
+        Party party5 = new Party()
+        {
+            Id = 5,
+            Price = 100000,
+            Name = "Graduation",
+            Description = "Adebisi's Graduation",
+        };
+
+        List<Party> parties = new List<Party>(3);
+
+        parties.Add(party1);
+        parties.Add(party2);
+        parties.Add(party3);
+        parties.Add(party4);
+
+        //Party lisparty = parties[3];
+        //foreach (var lisparty in parties)
+        //Console.WriteLine("Id = {0}, Name = {1}, Description = {2}, Price = {3}", lisparty.Id, lisparty.Name, lisparty.Description, lisparty.Price);
+
+        Party p  = parties.Find(x => x.Price > 150000);
+        Console.WriteLine("Id = {0}, Name = {1}, Description = {2}, Price = {3}", p.Id, p.Name, p.Description, p.Price);
+
+    }
+}
