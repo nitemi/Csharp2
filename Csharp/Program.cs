8397fc07096cd2838b4a911281ac2c5a1fa6e3f8
nitemi@ <blessedtgirl@gmail.com>
2023-12-04 15:01:42 +0100

    changes

--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -9,25 +9,44 @@ public class Program
     private static void Main(string[] args)
     {
         //Dictionary is mostly used for querying database, it contains key and value
-        //Party party1 = new Party() { Id = 1, Price = 100000, Name = "Birthday Celebration", Description = "Ade's birthday celebration" };
-        //Party party2 = new Party() { Id = 2, Price = 200000, Name = "Naming Ceremony", Description = "Mr & Mrs Adeyi naming celebration" };
-        //Party party3 = new Party() { Id = 3, Price = 450000, Name = "Corporate Event ", Description = "Walure Mananagement Event" };
-        //Party party4 = new Party() { Id = 4, Price = 250000, Name = "House Warming", Description = "Blessed House Opening", };
-        //Party party5 = new Party() { Id = 5, Price = 150000, Name = "Graduation", Description = "Adebisi's Graduation", };
+        Party party1 = new Party() { Id = 1, Price = 100000, Name = "Birthday Celebration", Description = "Ade's birthday celebration" };
+        Party party2 = new Party() { Id = 2, Price = 200000, Name = "Naming Ceremony", Description = "Mr & Mrs Adeyi naming celebration" };
+        Party party3 = new Party() { Id = 3, Price = 450000, Name = "Corporate Event ", Description = "Walure Mananagement Event" };
+        Party party4 = new Party() { Id = 4, Price = 250000, Name = "House Warming", Description = "Blessed House Opening", };
+        Party party5 = new Party() { Id = 5, Price = 150000, Name = "Graduation", Description = "Adebisi's Graduation", };
 
-        //Dictionary<int, Party> parties = new Dictionary<int, Party>();
-        //parties.Add(party1.Price, party1);
-        //parties.Add(party2.Price, party2);
-        //parties.Add(party3.Price, party3);
-        //parties.Add(party4.Price, party4);
-        //parties.Add(party5.Price, party5);
+        //stack : lastinFirstout
+        Stack<Party> stack = new Stack<Party>();
+        stack.Push(party1);
+        stack.Push(party2);
+        stack.Push(party3);
+        stack.Push(party4);
+        stack.Push(party5);
 
-        //foreach (KeyValuePair<int, Party> pair in parties)z
-        //{
-        //    Console.WriteLine("Key" + pair.Key);
-        //    //var party = pair.Value;
-        //    //Console.WriteLine("Id = {0}, Name = {1}, Description = {2}, Price = {3}", party.Id, party.Name, party.Description, party.Price);
-        //}
+        var a1 = stack.Pop();
+        Console.WriteLine(a1.Name + "   " + a1.Price);
+        Console.WriteLine("Items left in the stack");
+
+        var a2 = stack.Pop();
+        Console.WriteLine(a2.Name + "   " + a2.Price);
+        Console.WriteLine("Items left in the stack");
+
+        var a3 = stack.Pop();
+        Console.WriteLine(a3.Name + "   " + a3.Price);
+        Console.WriteLine("Items left in the stack");
+
+        var a4 = stack.Pop();
+        Console.WriteLine(a4.Name + "   " + a4.Price);
+        Console.WriteLine("Items left in the stack");
+
+        var a5 = stack.Pop();
+        Console.WriteLine(a5.Name + "   " + a5.Price);
+        Console.WriteLine("Items left in the stack");
+
+
+
+
+
 
         WordsandMeaning words1 = new WordsandMeaning() { Words = "Noun", Meaning = "a naming word. E.g Abuja,Cat, Tade,Freedom" };
         WordsandMeaning words2 = new WordsandMeaning() { Words = "Verb", Meaning = "an action words.E.g eat, finish, drink, believes,dance" };
@@ -38,23 +57,50 @@ public class Program
         WordsandMeaning words7 = new WordsandMeaning() { Words = "Adjectives", Meaning = "describes noun or pronoun , e.g hot,lazy,beautiful" };
         WordsandMeaning words8 = new WordsandMeaning() { Words = "Interjection", Meaning = "are expressions that can stand on thier own or be contained within a sentence.E.g ah!, ouch!, whoops!" };
 
-        Dictionary<string, string> words = new Dictionary<string, string>();
-        words.Add(words1.Words, words1.Meaning);
-        words.Add(words2.Words, words2.Meaning);
-        words.Add(words3.Words, words3.Meaning);
-        words.Add(words4.Words, words4.Meaning);
-        words.Add(words5.Words, words5.Meaning);
-        words.Add(words6.Words, words6.Meaning);
-        words.Add(words7.Words, words7.Meaning);
-        words.Add(words8.Words, words8.Meaning);
+        Stack<WordsandMeaning> words = new Stack<WordsandMeaning>();
+        words.Push(words1);
+        words.Push(words2);
+        words.Push(words3);
+        words.Push(words4);
+        words.Push(words5);
+        words.Push(words6);
+        words.Push(words7);
+        words.Push(words8);
 
-        foreach (KeyValuePair<string, string> pair in words)
-        {
-            Console.WriteLine("Key" + pair.Key);
-            var wordread = pair.Value;
-            Console.WriteLine(wordread);
+        var b1 = words.Pop();
+        Console.WriteLine(b1.Words + "   " + b1.Meaning);
+        Console.WriteLine("Items left in the stack");
 
-        }
+        var b2 = words.Pop();
+        Console.WriteLine(b2.Words + "   " + b2.Meaning);
+        Console.WriteLine("Items left in the stack");
+
+        var b3 = words.Pop();
+        Console.WriteLine(b3.Words + "   " + b3.Meaning);
+        Console.WriteLine("Items left in the stack");
+
+        var b4 = words.Pop();
+        Console.WriteLine(b4.Words + "   " + b4.Meaning);
+        Console.WriteLine("Items left in the stack");
+
+        var b5 = words.Pop();
+        Console.WriteLine(b5.Words + "   " + b5.Meaning);
+        Console.WriteLine("Items left in the stack");
+
+        var b6 = words.Pop();
+        Console.WriteLine(b6.Words + "   " + b6.Meaning);
+        Console.WriteLine("Items left in the stack");
+
+        var b7 = words.Pop();
+        Console.WriteLine(b7.Words + "   " + b7.Meaning);
+        Console.WriteLine("Items left in the stack");
+
+        var b8 = words.Pop();
+        Console.WriteLine(b8.Words + "   " + b8.Meaning);
+        Console.WriteLine("Items left in the stack");
+
+
+    }
 
     }
 }
