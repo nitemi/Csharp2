50dd016218555a850a28c608aa6b8b190c32fa4f
nitemi@ <blessedtgirl@gmail.com>
2023-12-06 17:50:35 +0100

    cng

--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -8,8 +8,10 @@ using System.Reflection.PortableExecutable;
 public class Program
 {
     //joining
-    //Grouping
-    //Ordering
+    //Convertion method
+    //Element operators
+    //Aggregate
+    //Quantifier
 
     //Join:In LINQ, Join operators are used for integrating two data source into one data source
     //which shares some common attributes
@@ -23,87 +25,38 @@ public class Program
     //Ordering
     private static void Main(string[] args)
     {
-        //Ordering
-        //orderby
-        //orderbydescending
-        //thenby
-        //thenbydecending
-        //var vend = Vendors.GetAllVendors()
-        //            .OrderBy(v => v.Id)
-        //            .ThenBy(v => v.Name)
-        //            .ToList();
 
-        //foreach (var v in vend)
-        //{
-        //    Console.WriteLine("Name:" + v.Name + "\t" + "Id:" + v.Id);
-        //}
-
-        //Grouping
-        //groupby
+        //innerjoin
         //query syntax
-        var info = (from vn in Vendors.GetAllVendors()
-                    group vn by vn.Id);
+
+        var info = from v in Vendors.GetAllVendors()
+                   join p in Planners.GetAllPlanners()
+                   on v.Id equals p.EmailId
+                   select new
+                   {
+
+                       VendorsId = v.Id,
+                       VendorsName = v.Name,
+                       PlannersName = p.Name,
+                   };
 
         //method syntax
-        var info1 = Vendors.GetAllVendors().GroupBy(v => v.Id);
-
-        foreach ( var data in info)
-        {
-            Console.WriteLine(data.Key + ":" + data.Count());
-            foreach ( var v in data)
+        var info1 = Vendors.GetAllVendors()
+            .Join(Planners.GetAllPlanners(),
+            p => p.Id,
+            v => v.EmailId,
+            (p, v) => new
             {
-                Console.WriteLine("Name:" + v.Name);
-            }
-        }
-        
+                VendorsId = p.Id,
+                VendorsName = p.Name,
+                PlannersName = v.Name,
+            });
 
-        //set operator
-        //Distinct
-        //Except
-        //Intersect
-        //union
-        Console.WriteLine("------------");
-        var datainfo = new List<string> { "ade", "femi", "ife","beloved","femi", "williams", "uchechi" };
-        var datainfo1 = new List<string> { "gift","ade", "femi","beloved", "emmanuel", "adeyemi", "euchaura" };
-
-        //method syntax
-        var record = datainfo.Distinct().ToList();
-
-        //query syntax
-        var record1 = (from nil in datainfo select nil).Distinct();
-            foreach ( var v in record)
+        foreach ( var record in info1 )
         {
-            Console.WriteLine(v);
+            Console.WriteLine("VendorsId:  {0}, \tVendorsName:  {1}, \tPlannersName:  {2}", record.VendorsId, record.VendorsName, record.PlannersName);
         }
-        Console.WriteLine("---------");
-
-        //Except
-        //method syntax
-        var record2 = datainfo.Except(datainfo1).ToList();
-            foreach ( var v1 in record2)
-        {
-            Console.WriteLine(v1);
-        }
-        Console.WriteLine("***********");
-
-        //intersect
-        //method syntax
-        var record3 = datainfo1.Intersect(datainfo).ToList();
-        foreach (var v2 in record3)
-        {
-            Console.WriteLine(v2);
-        }
-        Console.WriteLine("**********");
-
-        //union
-        //method syntax
-        var record4 = datainfo.Union(datainfo1).ToList();
-        foreach ( var v3 in record4)
-        {
-            Console.WriteLine(v3);
-        }
-            Console.ReadKey();     
-        
+                  
 
     }
 }
--- Csharp/Vendors.cs
+++ Csharp/Vendors.cs
@@ -17,16 +17,16 @@ namespace AdvancedCsharp
             return new List<Vendors>()
             {
                 //public static List<Vendors> vendor = new List<Vendors>{}
-                new Vendors { Id = 1, Name = "Toky's cakesand more", Description = "Cake Vendor" },
+                new Vendors { Id = 1, Name = "Toke's  cakes  and  more", Description = "Cake Vendor" },
                 new Vendors { Id = 2, Name = "Saveth Catering Services", Description = "Food Vendor" },
-                new Vendors { Id = 3, Name = "Blue Ribbon", Description = "Venue Vendor" },
-                new Vendors { Id = 4, Name = "Precious cakes", Description = "Cake Vendor" },
-                new Vendors { Id = 5, Name = "B&G Catering Services", Description = "Food Vendor" },
-                new Vendors { Id = 1, Name = "MC Kay & Crew", Description = "Entertainment Vendor" },
-                new Vendors { Id = 5, Name = "Swift cars", Description = "Logistics Vendor" },
-                new Vendors { Id = 4, Name = "The Podium Event Centre", Description = "Venue Vendor" },
-                new Vendors { Id = 2, Name = "TK Traditional Attire", Description = "Traditional Cloth Vendor" },
-                new Vendors { Id = 2, Name = "Ashake Corporate Ushers", Description = "Ushers Vendor" }
+                new Vendors { Id = 3, Name = "Blue Ribbon Event Centre", Description = "Venue Vendor" },
+                new Vendors { Id = 4, Name = "Precious cakes&surprises", Description = "Cake Vendor" },
+                new Vendors { Id = 5, Name = "B&G  CateringandServices", Description = "Food Vendor" },
+                new Vendors { Id = 1, Name = "MC  KayTy  Crew and Crew", Description = "Entertainment Vendor" },
+                new Vendors { Id = 5, Name = "Swift Cars Transportater", Description = "Logistics Vendor" },
+                new Vendors { Id = 4, Name = "The Podium Event  Centre", Description = "Venue Vendor" },
+                new Vendors { Id = 2, Name = "TimK  Traditional Attire", Description = "Traditional Cloth Vendor" },
+                new Vendors { Id = 2, Name = "Ashake Corporate  Ushers", Description = "Ushers Vendor" }
             };
         }
     }
